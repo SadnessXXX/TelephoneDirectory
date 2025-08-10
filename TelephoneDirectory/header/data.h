d13fbb5fba877d6a42af0f0f6ac48676733c7763
Sadness <153358364+SadnessXXX@users.noreply.github.com>
2025-08-10 22:16:01 +0300

    1

---
+++ TelephoneDirectory/header/data.h
@@ -0,0 +1,12 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include "defines.h"
+
+typedef struct {
+	char FullName[NAME_LENGHT];
+	char Phone[PHONE_LENGHT];
+} DATA;
+
+
+#endif // !DATA_H
---
+++ TelephoneDirectory/header/defines.h
@@ -0,0 +1,7 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define NAME_LENGHT 50
+#define PHONE_LENGHT 20
+
+#endif // !DEFINES_H
