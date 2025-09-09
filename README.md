c065f69ab4eda3a3e9b8709477e893cc2868a997
Sadness <153358364+SadnessXXX@users.noreply.github.com>
2025-09-09 14:45:22 +0300

    ending

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # TelephoneDirectory
+
+A simple phone book project written in C.
+
+## Description
+
+This phone book allows you to:
+
+- Add new records (name + phone number)
+- Show all records
+- Delete a record
+- Search for records by name or phone number
+- Manage (edit) records
+
+Data is stored in a dynamic array in memory.
+
+## Compilation
+
+Using GCC:
+
+```bash
+gcc src/*.c -o phonebook
--- TelephoneDirectory/sourse/data.c
+++ TelephoneDirectory/sourse/data.c
@@ -8,7 +8,7 @@
 void addRecord(PhoneBook* book){
 	
 	
-	char name[NAME_LENGTH];
+	char name [NAME_LENGTH];
 	char phone[PHONE_LENGTH];
 
 	printf("Write the name: ");
@@ -46,11 +46,11 @@ void removeRecord(PhoneBook* book) {
 	char data[20];
 	
 	printf("Enter name or phone number to delete: ");
-	(void)scanf("%s", &data);
+	(void)scanf("%s", data);
 
 	for (int i = 0; i < book->size; ++i) {
 		if (strcmp(book->entryList[i].FullName, data) == 0 ||
-			strcmp(book->entryList[i].Phone, data) == 0)	{
+			strcmp(book->entryList[i].Phone,	data) == 0)	{
 
 			for (int j = i; j < book->size-1; ++j) {
 				book->entryList[j] = book->entryList[j + 1];
@@ -74,16 +74,16 @@ void removeRecord(PhoneBook* book) {
 }
 
 void searchRecord(PhoneBook* book) {
-	char pattern[20];
+	char pattern[5];
 
 	printf("Pattern for searching name and phone: ");
-	(void)scanf("%s", &pattern);
+	(void)scanf("%s", pattern);
 
 	system("cls");
 
 	for (int i = 0; i < book->size; ++i) {
 		if (strstr(book->entryList[i].FullName, pattern) != 0 ||
-			strstr(book->entryList[i].Phone, pattern)    != 0) {
+			strstr(book->entryList[i].Phone,	pattern) != 0) {
 			printf("Match found!\nName: %s\nPhone: %s\n\n", 
 				book->entryList[i].FullName, 
 				book->entryList[i].Phone);
@@ -93,6 +93,39 @@ void searchRecord(PhoneBook* book) {
 }
 
 void manageRecord(PhoneBook* book){
+	char pattern[5];
+
+	char newName[NAME_LENGTH];
+	char newPhone[PHONE_LENGTH];
+
+	printf("Pattern for replacing a record: ");
+	(void)scanf("%s", pattern);
+
+	printf("Write new name for record: ");
+	(void)scanf("%s", newName);
+
+	printf("Write new phone for record: ");
+	(void)scanf("%s", newPhone);
+
+
+	for (int i = 0; i < book->size; ++i) {
+		if (strstr(book->entryList[i].FullName, pattern) != 0 ||
+			strstr(book->entryList[i].Phone,	pattern) != 0) {
+
+			
+			//memset(book->entryList[i].FullName, 0, NAME_LENGTH);
+			//memset(book->entryList[i].Phone,	0, PHONE_LENGTH);
+
+			strncpy(book->entryList[i].FullName, newName, NAME_LENGTH-1);
+			strncpy(book->entryList[i].FullName, newName, NAME_LENGTH - 1);
+
+			printf("Match found!\nNew name: %s\nNew phone: %s\n\n",
+				book->entryList[i].FullName,
+				book->entryList[i].Phone);
+
+			break;
+		}
+	}
 
 	system("cls");
 }
